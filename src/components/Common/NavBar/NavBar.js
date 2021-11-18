a8a1ce8590f878801e08fad4b1e4282dc4ec611e
bruyninxmaarten <maarten.bruyninx@gmail.com>
2021-11-18 08:06:21 +0100

    Fix navbar links

--- src/components/Common/NavBar/NavBar.js
+++ src/components/Common/NavBar/NavBar.js
@@ -7,9 +7,9 @@ class Navbar extends Component {
             <div className="navbar">
                 <div>Maarten Bruyninx</div>
                 <div className="items">
-                    <a className="hover-underline-animation" href="/">Home</a>
-                    <a className="hover-underline-animation" href="/#about">About</a>
-                    <a className="hover-underline-animation" href="/#contact">Contact</a>
+                    <a className="hover-underline-animation" href="/italent">Home</a>
+                    <a className="hover-underline-animation" href="/italent/#about">About</a>
+                    <a className="hover-underline-animation" href="/italent/#contact">Contact</a>
                     <a className="hover-underline-animation" href="/cv">CV</a>
                     <a className="hover-underline-animation" href="/blog">Blog</a>
                 </div>
